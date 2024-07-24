6b4a6298ff3374d696a6b975ce639615398b2cf2
Adi Poluri <52992596+adipoluri@users.noreply.github.com>
2024-07-24 00:25:15 -0700

    removing redundant files

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,8 +1,114 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-}, { timestamps: true })
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {  //Passwords are stored as hashes and not in plaintext
+    type: String,
+    required: true
+  }
+})
 
+
+//SignUp validator
+userSchema.statics.signup = async function(email, password) {
+
+    // validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+  
+    const exists = await this.findOne({ email })
+  
+    if (exists) {
+        throw Error('Email already in use')
+    }
+  
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+  
+    const user = await this.create({ email, password: hash })
+  
+    return user
+
+}
+  
+
+//LogIn validator
+userSchema.statics.login = async function(email, password) {
+  
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+  
+    const user = await this.findOne({ email })
+    if (!user) {
+        throw Error('Incorrect email')
+    }
+  
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+        throw Error('Incorrect password')
+    }
+  
+    return user
+}
+  
 module.exports = mongoose.model('User', userSchema)
+
+
+/*
+
+// Used to insert a new user into the database
+// Expects a username, email, passwordHash
+router.post('/auth/signup', async (req, res) => {
+  const db = getDb();
+
+  const { username, email, passwordHash } = req.body;
+
+  if (!username || !email || !passwordHash) {
+      return res.status(400).send('Missing required fields');
+  }
+
+  try {
+      const existingUser = await db.collection('users').findOne({ username });
+      if (existingUser) {
+          return res.status(409).send('Username already exists');
+      }
+
+      const newUser = {
+          username,
+          email,
+          passwordHash,
+          ingredients: [],
+          recipes: [],
+          favorites: []
+      };
+
+      await db.collection('users').insertOne(newUser);
+      
+      console.log('User created successfully')
+      res.status(201).send('User created successfully');
+
+  } catch (error) {
+      console.error('Error creating user:', error);
+      res.status(500).send('Error creating user');
+  }
+});
+
+
+
+*/

ce6def8f6db838f62ece59a4b6a93455e8a310c2
Adi Poluri <adipoluri@gmail.com>
2024-07-27 08:19:34 -0700

    Fixed consts

--- frontend/src/consts.js
+++ frontend/src/consts.js
@@ -1,2 +1,5 @@
+// export const API_URL = "http://localhost:6390/api";
+// export const env = "test";
+
 export const API_URL = "https://pantrypal-09-server.onrender.com/api";
-export const env = "test";
+export const env = "prod";

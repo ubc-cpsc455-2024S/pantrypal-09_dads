f4a61788e62d1a21b43ab17d3f8bc575ab5275fd
Adi Poluri <adipoluri@gmail.com>
2024-07-27 09:06:13 -0700

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -26,6 +26,9 @@ const loginUser = async (req, res) => {
 const signupUser = async (req, res) => {
   const {email, password} = req.body
 
+  // disable signup for workshop demo
+  res.status(500).json({error: "Signup Disabled for Workshop Demo to prevent spam lol"})
+
   try {
     const user = await User.signup(email, password)
 
